5168732cd04d45acd56e57fb859713ba03599963
Adi7893 <amakwan9@my.centennialcollege.ca>
2022-10-07 18:25:26 -0400

    completed third party express middleware

--- server.js
+++ server.js
@@ -1,20 +1,41 @@
-// immport express
+// third party modules
 import express from "express";
+import cookieParser from "cookie-parser";
+import logger from 'morgan';
+import session from "express-session";
 
+// ES modules fix for __dirname
+import path, {dirname} from 'path';
+import {fileURLToPath} from 'url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
 //instantiate app-server
 const app = express();
 
+//setup view engine 
+app.set('views', path.join(__dirname,'/views'));
+app.set('views engine', 'ejs');
+
+app.use(looger('dev'));
+app.use(express.jason());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(session({
+    secret: 'Mysecret',
+    saveUninitialized: false,
+    resave: false
+}));
 //custom middleware
 function helloWorld(req, res, next)
 {
     res.setHeader('Content-Type', 'text/plain');
-    res.end('hello world')
+    res.end('hello world');
 }
 
 function byeWorld(req, res, next)
 {
     res.setHeader('Content-Type','text/plain');
-    res.end('Good bye World')
+    res.end('Good bye World');
 }
 
 //add middleware to connect application
