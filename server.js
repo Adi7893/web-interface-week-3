d17249a0700b32d1bb013dd01e5549050da0c228
Adi7893 <amakwan9@my.centennialcollege.ca>
2022-10-07 18:32:50 -0400

    another minor change

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.set('views', path.join(__dirname,'/views'));
 app.set('views engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(express.jason());
+app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
